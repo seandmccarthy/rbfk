6124b970831225e210c43aacd812966a70d09a0e
Sean McCarthy <sean@clanmccarthy.net>
2016-03-21 22:45:48 +1100

    Adding more thorough Ook and Spoon to Brainfuck conversion tests

--- spec/ook_to_bf_spec.rb
+++ spec/ook_to_bf_spec.rb
@@ -4,4 +4,32 @@ describe 'Ook to Brainfuck' do
   it 'translates increment' do
     expect(BrainFuck.ook_to_bf('Ook. Ook.')).to eq '+'
   end
+
+  it 'translates decrement' do
+    expect(BrainFuck.ook_to_bf('Ook! Ook!')).to eq '-'
+  end
+
+  it 'translates move pointer left' do
+    expect(BrainFuck.ook_to_bf('Ook. Ook?')).to eq '>'
+  end
+
+  it 'translates move pointer right' do
+    expect(BrainFuck.ook_to_bf('Ook? Ook.')).to eq '<'
+  end
+
+  it 'translates read' do
+    expect(BrainFuck.ook_to_bf('Ook. Ook!')).to eq ','
+  end
+
+  it 'translates print' do
+    expect(BrainFuck.ook_to_bf('Ook! Ook.')).to eq '.'
+  end
+
+  it 'translates loop start' do
+    expect(BrainFuck.ook_to_bf('Ook! Ook?')).to eq '['
+  end
+
+  it 'translates loop end' do
+    expect(BrainFuck.ook_to_bf('Ook? Ook!')).to eq ']'
+  end
 end
--- spec/spoon_to_bf_spec.rb
+++ spec/spoon_to_bf_spec.rb
@@ -8,4 +8,28 @@ describe 'Spoon to Brainfuck' do
   it 'translates decrement' do
     expect(BrainFuck.spoon_to_bf('000')).to eq '-'
   end
+
+  it 'translates move pointer left' do
+    expect(BrainFuck.spoon_to_bf('010')).to eq '>'
+  end
+
+  it 'translates move pointer right' do
+    expect(BrainFuck.spoon_to_bf('011')).to eq '<'
+  end
+
+  it 'translates read' do
+    expect(BrainFuck.spoon_to_bf('0010110')).to eq ','
+  end
+
+  it 'translates print' do
+    expect(BrainFuck.spoon_to_bf('001010')).to eq '.'
+  end
+
+  it 'translates loop start' do
+    expect(BrainFuck.spoon_to_bf('00100')).to eq '['
+  end
+
+  it 'translates loop end' do
+    expect(BrainFuck.spoon_to_bf('0011')).to eq ']'
+  end
 end
